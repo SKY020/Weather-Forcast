bcf8182a062d3d0303856d696d11c03be67e1950
Shiv Kumar Yadav <shivamyadavsy020@gmail.com>
2023-02-05 18:29:59 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,83 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async(event) => {
+    event.preventDefault();
+
+    let cityVal = cityName.value;
+
+    if(cityVal === ""){
+        city_name.innerText = `Plz write the name before search`;
+        datahide.classList.add("data_hide");
+    }else{
+
+        try{
+           
+
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=22019221e580bf75972661e8922e99c6`
+            const response = await fetch(url);
+
+            const data = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+            console.log(tempMood);
+
+            //condition to check sunny or cloudy
+            if (tempMood == "Clear") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempMood == "Clouds") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+            } else if (tempMood == "Rain") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+
+            }
+            datahide.classList.remove('data_hide');
+            cityVal = "";
+       
+        }catch{
+            cityVal = " ";
+            datahide.classList.add("data_hide");
+            city_name.innerText =  `please enter the proper city name`;
+            console.log('please add the proper city name');
+        }
+        
+    }
+}
+
+var date  = new Date();
+const todaydate = document.querySelector('.today_date');
+const todayday = document.querySelector('.day');
+const noofday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function printDate(){
+    var day = date.getDate();
+    var month  = date.getMonth();
+    var year = date.getFullYear();
+    todaydate.innerHTML = day + " / " + month + " / "+ year;
+}
+printDate();
+
+function printDay(){
+ var date = new Date();
+ var today = date.getDay();
+ var day = noofday[today];
+ todayday.innerHTML = day;
+}
+printDay();
+
+
+submitBtn.addEventListener('click', getInfo);
